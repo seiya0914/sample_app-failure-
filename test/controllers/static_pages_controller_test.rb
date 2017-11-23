aa03c5e196bc782abc5cab4730ccfd23638ba20c
seiya0914 <boy0914@gmail.com>
2017-11-23 11:06:17 +0000

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,11 +7,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get help" do
     get static_pages_help_url
     assert_response :success
